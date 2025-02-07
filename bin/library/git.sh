47f62434c145dc68c9c4713d9418900b9364d52f
default-writer <funcelot@outlook.com>
2025-02-07 11:41:48 +0300

    added base repo scripts

---
+++ bin/library/git.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+if [[ "${BASHOPTS}" != *extdebug* ]]; then
+    set -e
+fi
+
+err_report() {
+    echo "ERROR: $0:$*"
+    exit 8
+}
+
+pwd=$(pwd)
+
+if [[ "${BASHOPTS}" != *extdebug* ]]; then
+    trap 'err_report $LINENO' ERR
+fi
+
+function submodule-install() {
+    local pwd
+
+    pwd=$(get-cwd)
+
+    if [[ ! -d  "${pwd}/.git/modules/$2" ]]; then
+        git submodule add -f "$1" "$2"
+        git submodule init
+        git submodule update --recursive --remote git --no-rebase
+    fi
+
+    git pull origin main --recurse-submodules --quiet
+}
+
+function submodule-uninstall() {
+    local pwd
+
+    pwd=$(get-cwd)
+
+    if [[ -d "${pwd}/$2" ]]; then
+        git submodule deinit -f "$2"
+        rm -rf "${pwd}/$2"
+    fi
+
+    if [[ -d "${pwd}/.git/modules/$2" ]]; then
+        rm -rf "${pwd}/.git/modules/$2"
+    fi
+}
+
+export -f submodule-install
+export -f submodule-uninstall
